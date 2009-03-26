e9511d43827293972bde8966d4191bc06db4534d
Phillip Berndt <phillip.berndt@gmail.com>
2009-03-26 12:42:49 +0100

    Improved performance on fullscreen mouse based image movement

--- pqiv.c
+++ pqiv.c
@@ -1965,13 +1965,18 @@ gint mouseMotionCb(GtkWidget *widget, GdkEventMotion *event, gpointer data) {
 		scrx = gdk_screen_get_width(screen) / 2;
 		scry = gdk_screen_get_height(screen) / 2;
 
+		// Don't perform too small movement (Improves performance a lot!)
+		if(abs(event->x - scrx) + abs(event->y - scry) < 4) {
+			return 0;
+		}
+
 		moveX += event->x - scrx;
 		moveY += event->y - scry;
 		gdk_display_warp_pointer(gdk_display_get_default(),
 			gdk_display_get_default_screen(gdk_display_get_default()), scrx, scry);
 
 		resizeAndPosWindow();
-		displayImage();
+		// displayImage();
 	}
 	#ifndef NO_COMPOSITING
 	else if(optionHideChessboardLevel == 3) {
