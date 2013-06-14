phillipberndt/pqiv

commit ed58a472f1d402ecefbfe2bc07dc6780fc44a824

Fixed no-scaling mode
My implementation sometimes ignored the no-scaling setting, because some
window managers ignore requests for window sizes larger that the screen
size. After this commit, in no-scaling mode, images are always displayed
at 100% if no-scaling is enabled, regarless of the window size. I also
disabled in-window-centering in this mode, because this led to funny
displayements at least in xfwm.

Related to issue #4

pqiv.c
@@ -772,7 +772,7 @@ gboolean image_loaded_handler(gconstpointer info_text) {/*{{{*/
 			}
 			// In both cases: If the height exceeds 80% screen size, scale
 			// down
-			if(image_height * current_scale_level > screen_height * .8) {
+			if(option_scale > 0 && image_height * current_scale_level > screen_height * .8) {
 				current_scale_level = screen_height * .8 / image_height;
 			}
 		}
@@ -1673,8 +1673,18 @@ gboolean window_draw_callback(GtkWidget *widget, cairo_t *cr, gpointer user_data
 		int image_width = cairo_image_surface_get_width(CURRENT_FILE->image_surface);
 		int image_height = cairo_image_surface_get_height(CURRENT_FILE->image_surface);
 
-		int x = (main_window_width - current_scale_level * image_width) / 2;
-		int y = (main_window_height - current_scale_level * image_height) / 2;
+		int x;
+		int y;
+
+		if(option_scale > 0) {
+			x = (main_window_width - current_scale_level * image_width) / 2;
+			y = (main_window_height - current_scale_level * image_height) / 2;
+		}
+		else {
+			// When scaling is disabled always use the upper left corder to avoid
+			// problems with window managers ignoring the large window size request.
+			x = y = 0;
+		}
 
 		cairo_save(cr);
 
@@ -1745,7 +1755,13 @@ void set_scale_level_to_fit() {/*{{{*/
 		int image_height = cairo_image_surface_get_height(CURRENT_FILE->image_surface);
 
 		current_scale_level = 1.0;
-		if(option_scale > 0 || !main_window_in_fullscreen || scale_override) {
+
+		// Only scale if scaling is not disabled. The alternative is to also
+		// scale for no-scaling mode if (!main_window_in_fullscreen ||
+		// scale_override). This effectively disables the no-scaling mode
+		// in non-fullscreen. I implemented that this way, but changed it
+		// per user request.
+		if(option_scale > 0) {
 			if(option_scale > 1 || scale_override) {
 				// Scale up
 				if(image_width * current_scale_level < main_window_width) {

commit 8f984cc6300cb9898db96c70e1a5e6afb7e13034

Make resizing using +/- possible in no-scaling mode
This fixes a bug introduced by ed58a47

pqiv.c
@@ -1676,7 +1676,7 @@ gboolean window_draw_callback(GtkWidget *widget, cairo_t *cr, gpointer user_data
 		int x;
 		int y;
 
-		if(option_scale > 0) {
+		if(option_scale > 0 || main_window_in_fullscreen) {
 			x = (main_window_width - current_scale_level * image_width) / 2;
 			y = (main_window_height - current_scale_level * image_height) / 2;
 		}
@@ -1757,11 +1757,10 @@ void set_scale_level_to_fit() {/*{{{*/
 		current_scale_level = 1.0;
 
 		// Only scale if scaling is not disabled. The alternative is to also
-		// scale for no-scaling mode if (!main_window_in_fullscreen ||
-		// scale_override). This effectively disables the no-scaling mode
-		// in non-fullscreen. I implemented that this way, but changed it
-		// per user request.
-		if(option_scale > 0) {
+		// scale for no-scaling mode if (!main_window_in_fullscreen). This
+		// effectively disables the no-scaling mode in non-fullscreen. I
+		// implemented that this way, but changed it per user request.
+		if(option_scale > 0 || scale_override) {
 			if(option_scale > 1 || scale_override) {
 				// Scale up
 				if(image_width * current_scale_level < main_window_width) {
@@ -1896,7 +1895,7 @@ gboolean window_key_press_callback(GtkWidget *widget, GdkEventKey *event, gpoint
 		case GDK_KEY_plus:
 		case GDK_KEY_KP_Add:
 			current_scale_level *= 1.1;
-			if(current_scale_level > 1 && option_scale != 2) {
+			if((option_scale == 1 && current_scale_level > 1) || option_scale == 0) {
 				scale_override = TRUE;
 			}
 			if(main_window_in_fullscreen) {
@@ -1917,6 +1916,9 @@ gboolean window_key_press_callback(GtkWidget *widget, GdkEventKey *event, gpoint
 				break;
 			}
 			current_scale_level /= 1.1;
+			if((option_scale == 1 && current_scale_level > 1) || option_scale == 0) {
+				scale_override = TRUE;
+			}
 			if(main_window_in_fullscreen) {
 				gtk_widget_queue_draw(GTK_WIDGET(main_window));
 			}