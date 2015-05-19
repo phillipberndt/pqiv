phillipberndt/pqiv

commit 786319e6c8776c966d635b2bb77be201cc918f30

If no images can be loaded using -l, quit right away

pqiv.c
@@ -3776,6 +3776,19 @@ gboolean initialize_gui_callback(gpointer user_data) {/*{{{*/
 
 	return FALSE;
 }/*}}}*/
+gboolean initialize_gui_or_quit_callback(gpointer user_data) {/*{{{*/
+	if(!gui_initialized && initialize_image_loader()) {
+		initialize_gui();
+		gui_initialized = TRUE;
+	}
+
+	if(!file_tree_valid || bostree_node_count(file_tree) == 0) {
+		g_printerr("No images left to display.\n");
+		exit(1);
+	}
+
+	return FALSE;
+}/*}}}*/
 // }}}
 
 gboolean load_images_thread_update_info_text(gpointer user_data) {/*{{{*/
@@ -3812,6 +3825,8 @@ gpointer load_images_thread(gpointer user_data) {/*{{{*/
 			exit(1);
 		}
 	}
+
+	gdk_threads_add_idle(initialize_gui_or_quit_callback, NULL);
 
 	if(user_data != NULL) {
 		g_source_remove(event_source);

commit d518d7ae765dd88ca35659dd7501395d05a6fee6

Bugfix: 786319e broke non-lazy-loading

pqiv.c
@@ -3826,7 +3826,9 @@ gpointer load_images_thread(gpointer user_data) {/*{{{*/
 		}
 	}
 
-	gdk_threads_add_idle(initialize_gui_or_quit_callback, NULL);
+	if(option_lazy_load) {
+		gdk_threads_add_idle(initialize_gui_or_quit_callback, NULL);
+	}
 
 	if(user_data != NULL) {
 		g_source_remove(event_source);